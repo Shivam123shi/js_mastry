04ef78ac86887dcd8b405a976c2633ed65b64a2b
Shivam123shi <89355789+Shivam123shi@users.noreply.github.com>
2025-08-04 13:19:42 +0000

    second

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 262620
+let accountEmail="shiv@123.com"
+var accountPass="2345"
+accountCity="haryana"
+let accountState
+
+
+accountEmail="Shit@12.com"
+accountCity="faridabad"
+accountPass="7757"
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+console.log(accountId);
+
+
