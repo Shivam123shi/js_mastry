d2ecad7e9d17a3b49a8f9f728686d31d63ea9ab2
Shivam123shi <89355789+Shivam123shi@users.noreply.github.com>
2025-08-05 04:29:47 +0000

    third

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+console.log("Shiv")
+
+console.log(4+3)
+
+let name="Shivansh"
+let age=69
+let isloggedIn=true
+
+//number= 2 to power 53
+//BigInt=for larger numbers
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefined=not given a value we just leave it
+//Symbol=unique
+console.log(typeof("true"))
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
+
