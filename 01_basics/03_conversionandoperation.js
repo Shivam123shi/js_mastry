6d6902c8a704e854ba8b10835ce4c671ab799a97
Shivam123shi <89355789+Shivam123shi@users.noreply.github.com>
2025-08-06 19:42:30 +0000

    fourth

--- 01_basics/03_conversionandoperation.js
+++ 01_basics/03_conversionandoperation.js
@@ -1,24 +1,34 @@
-let score=true
+// let score=true
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 //"33"=33
 //"33abc"=NAN
 //true=1;false=0
 
 
-let isloggedIn=1
-let bollIsLoogedIn=Boolean(isloggedIn)
-console.log(bollIsLoogedIn)
- //1=true,0=false
- //""=false
- //"shiva"=true
- let someNumber=33
- let stringNumber= String(someNumber)
- console.log(stringNumber)
- console.log(typeof stringNumber)
+// et isloggedIn=1
+// let bollIsLoogedIn=Boolean(isloggedIn)
+// console.log(bollIsLoogedIn)
+//  //1=true,0=false
+//  //""=false
+//  //"shiva"=true
+//   let someNumber=33
+//  let stringNumber= String(someNumber)
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber)l
+// let str1=2
+// let str2=4
+// str3=str1+str2
+// //console.log(str3);
+// console.log(true)
+// console.log(+true)
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
